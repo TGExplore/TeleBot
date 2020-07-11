a627cfebd2ae2ecaad894f5c208eb166db82e400
ViruZs <52099763+TGExplore@users.noreply.github.com>
2020-07-11 13:25:11 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -15,14 +15,14 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet, check pinned 
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
     
-    await borg.send_message(alive.chat_id, """**Welcome To TeleBot **
-**`Hey! I'm alive. All systems online and functioning normally!`**
-` 🔸 Telethon version:` **1.15.0**\n` 🔹 Python:` **3.8.3**
-` 🔸 More info:` [TeleBot](https://telegra.ph/TeleBot-07-08)
-` 🔹 Bot created by:` [Aditya 🇮🇳](tg://user?id=719195224)
-` 🔸 Database Status:` **All OK 👌!**
-` 🔹 My pro owner`: {DEFAULTUSER}
-[✨ GitHub Repository ✨](https://github.com/xditya/TeleBot)""", link_preview = False)
+    await borg.send_message(alive.chat_id, f"**Welcome To TeleBot **"
+f"**`Hey! I'm alive. All systems online and functioning normally!`**"
+f"` 🔸 Telethon version:` **1.15.0**\n` 🔹 Python:` **3.8.3**"
+f"` 🔸 More info:` [TeleBot](https://telegra.ph/TeleBot-07-08)"
+f"` 🔹 Bot created by:` [Aditya 🇮🇳](tg://user?id=719195224)"
+f"` 🔸 Database Status:` **All OK 👌!**"
+f"` 🔹 My pro owner`: {DEFAULTUSER}"
+f"[✨ GitHub Repository ✨](https://github.com/xditya/TeleBot)", link_preview = False)
 
     #await borg.forward_messages(alive.chat_id, 167, -1001195912925)
     await alive.delete()
