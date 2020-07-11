b06866c97790cfb0398a5611d71c24fb9391b8f5
ViruZs <52099763+TGExplore@users.noreply.github.com>
2020-07-11 14:44:06 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -17,17 +17,17 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet, check pinned 
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
     
-req = requests.get("https://telegra.ph/file/0670190de8e3bddea6d95.png")
-req.raise_for_status()
-file = BytesIO(req.content)
-file.seek(0)
-img = Image.open(file)
-with BytesIO() as sticker:
-    img.save(sticker, "webp")
-    sticker.name = "sticker.webp"
-    sticker.seek(0)
-    #print(sticker)
-    await borg.send_message(alive.chat_id, f"**Welcome To TeleBot **\n\n"
+    req = requests.get("https://telegra.ph/file/0670190de8e3bddea6d95.png")
+    req.raise_for_status()
+    file = BytesIO(req.content)
+    file.seek(0)
+    img = Image.open(file)
+    with BytesIO() as sticker:
+        img.save(sticker, "webp")
+        sticker.name = "sticker.webp"
+        sticker.seek(0)
+        #print(sticker)
+        await borg.send_message(alive.chat_id, f"**Welcome To TeleBot **\n\n"
 f"**`Hey! I'm alive. All systems online and functioning normally!`**\n\n"
 f"` 🔸 Telethon version:` **1.15.0**\n` 🔹 Python:` **3.8.3**\n"
 f"` 🔸 More info:` [TeleBot](https://telegra.ph/TeleBot-07-08)\n"
@@ -35,5 +35,5 @@ f"` 🔹 Bot created by:` [Aditya 🇮🇳](tg://user?id=719195224)\n"
 f"` 🔸 Database Status:` **All OK 👌!**\n"
 f"` 🔹 My pro owner`: {DEFAULTUSER}\n"
 "[✨ GitHub Repository ✨](https://github.com/xditya/TeleBot)", link_preview = False)
-    await borg.send_file(alive.chat_id, file=sticker) 
-    await alive.delete()
+        await borg.send_file(alive.chat_id, file=sticker) 
+        await alive.delete()
